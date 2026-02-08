74588bb31be7e3fa804d9233cc43180ce9c26fba
Anne-Gaelle Colom <coloma@ncsmnwy3l32r4.home>
2026-02-08 17:28:04 +0000

    Add affix functionality to bootstrap-replacement.js
    - Implemented jQuery affix plugin to replace Bootstrap affix
    - Makes sidebar stick to viewport when scrolling
    - Fixes 'affix is not a function' error in Lessons pages

--- bootstrap-replacement.js
+++ bootstrap-replacement.js
@@ -131,4 +131,56 @@
         });
     };
 
+    // Affix - makes elements stick to viewport when scrolling
+    $.fn.affix = function(options) {
+        var defaults = {
+            offset: {
+                top: 0,
+                bottom: 0
+            }
+        };
+        var settings = $.extend(true, {}, defaults, options);
+
+        return this.each(function() {
+            var $element = $(this);
+            var $window = $(window);
+            var affixed = null;
+            var originalPosition = $element.css('position');
+            var originalTop = $element.css('top');
+            var elementOffsetTop = $element.offset().top;
+
+            function checkPosition() {
+                var scrollTop = $window.scrollTop();
+                var offsetTop = typeof settings.offset.top === 'function'
+                    ? settings.offset.top()
+                    : settings.offset.top;
+                var offsetBottom = typeof settings.offset.bottom === 'function'
+                    ? settings.offset.bottom()
+                    : settings.offset.bottom;
+
+                if (scrollTop > elementOffsetTop - offsetTop && affixed !== 'top') {
+                    // Affix to top
+                    affixed = 'top';
+                    $element.css({
+                        position: 'fixed',
+                        top: offsetTop + 'px'
+                    }).addClass('affix').removeClass('affix-top affix-bottom');
+                } else if (scrollTop <= elementOffsetTop - offsetTop && affixed !== null) {
+                    // Return to normal position
+                    affixed = null;
+                    $element.css({
+                        position: originalPosition,
+                        top: originalTop
+                    }).addClass('affix-top').removeClass('affix affix-bottom');
+                }
+            }
+
+            $window.on('scroll', checkPosition);
+            $window.on('resize', checkPosition);
+
+            // Initial check
+            checkPosition();
+        });
+    };
+
 })(jQuery);
